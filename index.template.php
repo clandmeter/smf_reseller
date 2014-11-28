900d5a642438e615c99eb5361bdc543fd157f7f0
Carlo Landmeter <clandmeter@gmail.com>
2014-11-28 10:58:02 +0100

    Move nav bar below header

--- index.template.php
+++ index.template.php
@@ -185,30 +185,6 @@ function template_body_above()
 	global $context, $settings, $options, $scripturl, $txt, $modSettings;
 
 	echo'
-<nav class="navbar navbar-default navbar-static-top" role="navigation">
-	<div class="container">
-		<div class="row">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand visible-xs" href="', $scripturl, '">', $context['forum_name'] ,'</a>
-			</div>
-			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				<ul class="nav navbar-nav">';
-				
-					// Show the menu here, according to the menu sub template.
-					template_menu();
-					
-				echo'
-				</ul>
-			</div>
-		</div>
-	</div>
-</nav>
 <header>
 	<div class="container">
 		<div class="row">
@@ -268,7 +244,32 @@ function template_body_above()
 			</div>
 		</div>
 	</div>
-</header>';
+</header>
+<nav class="navbar navbar-default navbar-static-top" role="navigation">
+        <div class="container">
+                <div class="row">
+                        <div class="navbar-header">
+                                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                                        <span class="sr-only">Toggle navigation</span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                </button>
+                                <a class="navbar-brand visible-xs" href="', $scripturl, '">', $context['forum_name'] ,'</a>
+                        </div>
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                                <ul class="nav navbar-nav">';
+
+                                        // Show the menu here, according to the menu sub template.
+                                        template_menu();
+
+                                echo'
+                                </ul>
+                        </div>
+                </div>
+        </div>
+</nav>
+';
 
 	// Show the navigation tree.
 	theme_linktree();
