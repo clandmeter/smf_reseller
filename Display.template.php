ef31237eebfc781d165a499404e33af636b63466
Carlo Landmeter <clandmeter@gmail.com>
2014-12-04 12:27:10 +0100

    ident the button arrays
    So they are actually readable

--- Display.template.php
+++ Display.template.php
@@ -115,14 +115,51 @@ function template_main()
 			<div id="pollmoderation">';
 
 		// Build the poll moderation button array.
-		$poll_buttons = array(
-			'vote' => array('test' => 'allow_return_vote', 'text' => 'poll_return_vote', 'image' => 'poll_options.gif', 'lang' => true, 'url' => $scripturl . '?topic=' . $context['current_topic'] . '.' . $context['start']),
-			'results' => array('test' => 'show_view_results_button', 'text' => 'poll_results', 'image' => 'poll_results.gif', 'lang' => true, 'url' => $scripturl . '?topic=' . $context['current_topic'] . '.' . $context['start'] . ';viewresults'),
-			'change_vote' => array('test' => 'allow_change_vote', 'text' => 'poll_change_vote', 'image' => 'poll_change_vote.gif', 'lang' => true, 'url' => $scripturl . '?action=vote;topic=' . $context['current_topic'] . '.' . $context['start'] . ';poll=' . $context['poll']['id'] . ';' . $context['session_var'] . '=' . $context['session_id']),
-			'lock' => array('test' => 'allow_lock_poll', 'text' => (!$context['poll']['is_locked'] ? 'poll_lock' : 'poll_unlock'), 'image' => 'poll_lock.gif', 'lang' => true, 'url' => $scripturl . '?action=lockvoting;topic=' . $context['current_topic'] . '.' . $context['start'] . ';' . $context['session_var'] . '=' . $context['session_id']),
-			'edit' => array('test' => 'allow_edit_poll', 'text' => 'poll_edit', 'image' => 'poll_edit.gif', 'lang' => true, 'url' => $scripturl . '?action=editpoll;topic=' . $context['current_topic'] . '.' . $context['start']),
-			'remove_poll' => array('test' => 'can_remove_poll', 'text' => 'poll_remove', 'image' => 'admin_remove_poll.gif', 'lang' => true, 'custom' => 'onclick="return confirm(\'' . $txt['poll_remove_warn'] . '\');"', 'url' => $scripturl . '?action=removepoll;topic=' . $context['current_topic'] . '.' . $context['start'] . ';' . $context['session_var'] . '=' . $context['session_id']),
-		);
+                $poll_buttons = array(
+                    'vote' => array(
+                        'test' => 'allow_return_vote',
+                        'text' => 'poll_return_vote',
+                        'image' => 'poll_options.gif',
+                        'lang' => true,
+                        'url' => $scripturl . '?topic=' . $context['current_topic'] . '.' . $context['start']
+                    ),
+                    'results' => array(
+                        'test' => 'show_view_results_button',
+                        'text' => 'poll_results',
+                        'image' => 'poll_results.gif',
+                        'lang' => true,
+                        'url' => $scripturl . '?topic=' . $context['current_topic'] . '.' . $context['start'] . ';viewresults'
+                    ),
+                    'change_vote' => array(
+                        'test' => 'allow_change_vote',
+                        'text' => 'poll_change_vote',
+                        'image' => 'poll_change_vote.gif',
+                        'lang' => true,
+                        'url' => $scripturl . '?action=vote;topic=' . $context['current_topic'] . '.' . $context['start'] . ';poll=' . $context['poll']['id'] . ';' . $context['session_var'] . '=' . $context['session_id']
+                    ),
+                    'lock' => array(
+                        'test' => 'allow_lock_poll',
+                        'text' => (!$context['poll']['is_locked'] ? 'poll_lock' : 'poll_unlock'),
+                        'image' => 'poll_lock.gif',
+                        'lang' => true,
+                        'url' => $scripturl . '?action=lockvoting;topic=' . $context['current_topic'] . '.' . $context['start'] . ';' . $context['session_var'] . '=' . $context['session_id']
+                    ),
+                    'edit' => array(
+                        'test' => 'allow_edit_poll',
+                        'text' => 'poll_edit',
+                        'image' => 'poll_edit.gif',
+                        'lang' => true,
+                        'url' => $scripturl . '?action=editpoll;topic=' . $context['current_topic'] . '.' . $context['start']
+                    ),
+                    'remove_poll' => array(
+                        'test' => 'can_remove_poll',
+                        'text' => 'poll_remove',
+                        'image' => 'admin_remove_poll.gif',
+                        'lang' => true,
+                        'custom' => 'onclick="return confirm(\'' . $txt['poll_remove_warn'] . '\');"',
+                        'url' => $scripturl . '?action=removepoll;topic=' . $context['current_topic'] . '.' . $context['start'] . ';' . $context['session_var'] . '=' . $context['session_id']
+                    ),
+                );
 		template_button_strip($poll_buttons);
 
 		echo '
@@ -157,14 +194,51 @@ function template_main()
 	}
 
 	// Build the normal button array.
-	$normal_buttons = array(
-		'reply' => array('test' => 'can_reply', 'text' => 'reply', 'image' => 'reply.gif', 'lang' => true, 'url' => $scripturl . '?action=post;topic=' . $context['current_topic'] . '.' . $context['start'] . ';last_msg=' . $context['topic_last_message'], 'active' => true),
-		'add_poll' => array('test' => 'can_add_poll', 'text' => 'add_poll', 'image' => 'add_poll.gif', 'lang' => true, 'url' => $scripturl . '?action=editpoll;add;topic=' . $context['current_topic'] . '.' . $context['start']),
-		'notify' => array('test' => 'can_mark_notify', 'text' => $context['is_marked_notify'] ? 'unnotify' : 'notify', 'image' => ($context['is_marked_notify'] ? 'un' : '') . 'notify.gif', 'lang' => true, 'custom' => 'onclick="return confirm(\'' . ($context['is_marked_notify'] ? $txt['notification_disable_topic'] : $txt['notification_enable_topic']) . '\');"', 'url' => $scripturl . '?action=notify;sa=' . ($context['is_marked_notify'] ? 'off' : 'on') . ';topic=' . $context['current_topic'] . '.' . $context['start'] . ';' . $context['session_var'] . '=' . $context['session_id']),
-		'mark_unread' => array('test' => 'can_mark_unread', 'text' => 'mark_unread', 'image' => 'markunread.gif', 'lang' => true, 'url' => $scripturl . '?action=markasread;sa=topic;t=' . $context['mark_unread_time'] . ';topic=' . $context['current_topic'] . '.' . $context['start'] . ';' . $context['session_var'] . '=' . $context['session_id']),
-		'send' => array('test' => 'can_send_topic', 'text' => 'send_topic', 'image' => 'sendtopic.gif', 'lang' => true, 'url' => $scripturl . '?action=emailuser;sa=sendtopic;topic=' . $context['current_topic'] . '.0'),
-		'print' => array('text' => 'print', 'image' => 'print.gif', 'lang' => true, 'custom' => 'rel="new_win nofollow"', 'url' => $scripturl . '?action=printpage;topic=' . $context['current_topic'] . '.0'),
-	);
+        $normal_buttons = array(
+            'reply' => array(
+                'test' => 'can_reply',
+                'text' => 'reply',
+                'image' => 'reply.gif',
+                'lang' => true,
+                'url' => $scripturl . '?action=post;topic=' . $context['current_topic'] . '.' . $context['start'] . ';last_msg=' . $context['topic_last_message'],
+                'active' => true
+            ),
+            'add_poll' => array('test' => 'can_add_poll',
+                'text' => 'add_poll',
+                'image' => 'add_poll.gif',
+                'lang' => true,
+                'url' => $scripturl . '?action=editpoll;add;topic=' . $context['current_topic'] . '.' . $context['start']
+            ),
+            'notify' => array(
+                'test' => 'can_mark_notify',
+                'text' => $context['is_marked_notify'] ? 'unnotify' : 'notify',
+                'image' => ($context['is_marked_notify'] ? 'un' : '') . 'notify.gif',
+                'lang' => true,
+                'custom' => 'onclick="return confirm(\'' . ($context['is_marked_notify'] ? $txt['notification_disable_topic'] : $txt['notification_enable_topic']) . '\');"',
+                'url' => $scripturl . '?action=notify;sa=' . ($context['is_marked_notify'] ? 'off' : 'on') . ';topic=' . $context['current_topic'] . '.' . $context['start'] . ';' . $context['session_var'] . '=' . $context['session_id']
+            ),
+            'mark_unread' => array(
+                'test' => 'can_mark_unread',
+                'text' => 'mark_unread',
+                'image' => 'markunread.gif',
+                'lang' => true,
+                'url' => $scripturl . '?action=markasread;sa=topic;t=' . $context['mark_unread_time'] . ';topic=' . $context['current_topic'] . '.' . $context['start'] . ';' . $context['session_var'] . '=' . $context['session_id']
+            ),
+            'send' => array(
+                'test' => 'can_send_topic',
+                'text' => 'send_topic',
+                'image' => 'sendtopic.gif',
+                'lang' => true,
+                'url' => $scripturl . '?action=emailuser;sa=sendtopic;topic=' . $context['current_topic'] . '.0'
+            ),
+            'print' => array(
+                'text' => 'print',
+                'image' => 'print.gif',
+                'lang' => true,
+                'custom' => 'rel="new_win nofollow"',
+                'url' => $scripturl . '?action=printpage;topic=' . $context['current_topic'] . '.0'
+            ),
+        );
 	// Allow adding new buttons easily.
 	call_integration_hook('integrate_display_buttons', array(&$normal_buttons));
 
@@ -652,15 +726,45 @@ function template_main()
 
 	// Show the lower breadcrumbs.
 	theme_linktree();
-
-	$mod_buttons = array(
-		'move' => array('test' => 'can_move', 'text' => 'move_topic', 'image' => 'admin_move.gif', 'lang' => true, 'url' => $scripturl . '?action=movetopic;topic=' . $context['current_topic'] . '.0'),
-		'delete' => array('test' => 'can_delete', 'text' => 'remove_topic', 'image' => 'admin_rem.gif', 'lang' => true, 'custom' => 'onclick="return confirm(\'' . $txt['are_sure_remove_topic'] . '\');"', 'url' => $scripturl . '?action=removetopic2;topic=' . $context['current_topic'] . '.0;' . $context['session_var'] . '=' . $context['session_id']),
-		'lock' => array('test' => 'can_lock', 'text' => empty($context['is_locked']) ? 'set_lock' : 'set_unlock', 'image' => 'admin_lock.gif', 'lang' => true, 'url' => $scripturl . '?action=lock;topic=' . $context['current_topic'] . '.' . $context['start'] . ';' . $context['session_var'] . '=' . $context['session_id']),
-		'sticky' => array('test' => 'can_sticky', 'text' => empty($context['is_sticky']) ? 'set_sticky' : 'set_nonsticky', 'image' => 'admin_sticky.gif', 'lang' => true, 'url' => $scripturl . '?action=sticky;topic=' . $context['current_topic'] . '.' . $context['start'] . ';' . $context['session_var'] . '=' . $context['session_id']),
-		'merge' => array('test' => 'can_merge', 'text' => 'merge', 'image' => 'merge.gif', 'lang' => true, 'url' => $scripturl . '?action=mergetopics;board=' . $context['current_board'] . '.0;from=' . $context['current_topic']),
-		'calendar' => array('test' => 'calendar_post', 'text' => 'calendar_link', 'image' => 'linktocal.gif', 'lang' => true, 'url' => $scripturl . '?action=post;calendar;msg=' . $context['topic_first_message'] . ';topic=' . $context['current_topic'] . '.0'),
-	);
+        $mod_buttons = array(
+                'move' => array(
+                    'test' => 'can_move',
+                    'text' => 'move_topic',
+                    'image' => 'admin_move.gif',
+                    'lang' => true,
+                    'url' => $scripturl . '?action=movetopic;topic=' . $context['current_topic'] . '.0'),
+                'delete' => array(
+                    'test' => 'can_delete',
+                    'text' => 'remove_topic',
+                    'image' => 'admin_rem.gif',
+                    'lang' => true,
+                    'custom' => 'onclick="return confirm(\'' . $txt['are_sure_remove_topic'] . '\');"',
+                    'url' => $scripturl . '?action=removetopic2;topic=' . $context['current_topic'] . '.0;' . $context['session_var'] . '=' . $context['session_id']),
+                'lock' => array(
+                    'test' => 'can_lock',
+                    'text' => empty($context['is_locked']) ? 'set_lock' : 'set_unlock',
+                    'image' => 'admin_lock.gif',
+                    'lang' => true,
+                    'url' => $scripturl . '?action=lock;topic=' . $context['current_topic'] . '.' . $context['start'] . ';' . $context['session_var'] . '=' . $context['session_id']),
+                'sticky' => array(
+                    'test' => 'can_sticky',
+                    'text' => empty($context['is_sticky']) ? 'set_sticky' : 'set_nonsticky',
+                    'image' => 'admin_sticky.gif',
+                    'lang' => true,
+                    'url' => $scripturl . '?action=sticky;topic=' . $context['current_topic'] . '.' . $context['start'] . ';' . $context['session_var'] . '=' . $context['session_id']),
+                'merge' => array(
+                    'test' => 'can_merge',
+                    'text' => 'merge',
+                    'image' => 'merge.gif',
+                    'lang' => true,
+                    'url' => $scripturl . '?action=mergetopics;board=' . $context['current_board'] . '.0;from=' . $context['current_topic']),
+                'calendar' => array(
+                    'test' => 'calendar_post',
+                    'text' => 'calendar_link',
+                    'image' => 'linktocal.gif',
+                    'lang' => true,
+                    'url' => $scripturl . '?action=post;calendar;msg=' . $context['topic_first_message'] . ';topic=' . $context['current_topic'] . '.0'),
+        );
 
 	// Restore topic. eh?  No monkey business.
 	if ($context['can_restore_topic'])
